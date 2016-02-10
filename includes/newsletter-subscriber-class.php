60ac8bc26b6ef5b813a536dccdf29d45e493f939
Iam <iam@gmial.com>
2016-02-10 20:48:13 +0300

    main php files

---
+++ includes/newsletter-subscriber-class.php
@@ -0,0 +1,46 @@
+<?
+
+
+ class Newsletter_Subscriber_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		parent::__construct(
+		'newsletter_subscriber_widget', // Base ID
+		__( 'My Pop Up Subscriber Form','ns_domain' ), // Name
+		array( 'description' => __( 'Just My Pop Up Subscriber Form','ns_domain' ), ) // Args
+	 );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+		echo '111';
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
---
+++ newsletter-subscriber.php
@@ -0,0 +1,25 @@
+<?
+/**
+ * Plugin Name: My Pop Up Subscriber Form
+ * Description: Just My Pop Up Subscriber Form
+ * Version: 0.1
+ * License: GPL2
+ */ 
+ 
+ //EXIT if Accessed Directly
+if(!defined('ABSPATH')){
+	exit;
+}
+ 
+//Load Scripts
+require_once(plugin_dir_path(__FILE__).'/includes/newsletter-subscriber-scripts.php');
+ //Load Class
+ require_once(plugin_dir_path(__FILE__).'/includes/newsletter-subscriber-class.php');
+ 
+ //Register Widget
+ function register_newsletter_subscriber(){
+	 register_widget('Newsletter_Subscriber_Widget');
+}
+ 
+add_action( 'widgets_init', 'register_newsletter_subscriber');
+ 
